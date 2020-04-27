2234095c237edc46f7ba172108c1eb0226565b04
João Victor <victorpow@live.com>
2020-04-26 21:16:27 -0300

    user can create a new task list - test and feature

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,29 @@ class ListsController < ActionController::Base
   def index
     @lists = List.all
   end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params) 
+    @list.user = current_user
+    return redirect_to @list,
+            notice: 'Registrado com sucesso'if @list.save
+
+    render :new
+
+  end
+
+  private
+
+  def list_params
+      params.require(:list).permit(:name, :description, :status)
+  end
+
 end
---
+++ spec/feature/lists/user_create_todo_list_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User create todo list' do
+  scenario 'Successfully' do
+    user = create(:user)
+
+    login_as(user, scope: :user)
+    visit root_path
+    click_on 'Lista de Tarefas'
+    click_on 'Nova Lista de Tarefa'
+    fill_in 'Name', with: 'Tarefas de casa'
+    fill_in 'Description', with: 'Tarefas que serão realizadas diariamente.'
+    select 'Collective', from: 'Status'
+    click_on 'Salvar'
+    
+    expect(page).to have_content('Tarefas de casa')
+    expect(page).to have_content('Tarefas que serão realizadas diariamente.')
+    expect(page).to have_content('collective')
+  end
+end
