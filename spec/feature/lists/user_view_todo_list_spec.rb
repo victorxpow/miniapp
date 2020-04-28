82f2dcb2f693be002a97ce9fc7ea780a4ca1b104
João Victor <victorpow@live.com>
2020-04-28 19:00:04 -0300

    user can favorite lists - test / feature at wip

--- spec/feature/lists/user_view_todo_list_spec.rb
+++ spec/feature/lists/user_view_todo_list_spec.rb
@@ -17,7 +17,7 @@ feature 'User view lists' do
     expect(page).to have_content(other_task.title)
   end
 
-  scenario 'and do not see personal lists' do
+  scenario 'and do not see private lists' do
     user = create(:user)
     list = create(:list, user: user)
     other_user = create(:user, email: 'other@test.com')
@@ -33,5 +33,24 @@ feature 'User view lists' do
     expect(page).not_to have_content(other_list.name)
     expect(page).not_to have_content(other_list.description)
     expect(page).not_to have_content(other_list.id)
+  end
+
+  scenario 'favorites' do
+    user = create(:user)
+    list = create(:list, user: user)
+    task = create(:task, title: 'Arcade', list: list)
+    other_task = create(:task, title: 'Robo', list: list)
+    favorite = create(:favorite, user: user, list: list)
+
+    login_as(user, scope: :user)
+    visit root_path
+    click_on 'Lista de Tarefas'
+    click_on 'Favoritos'
+
+    expect(page).to have_content(list.name)
+    expect(page).to have_content(list.description)
+    expect(page).to have_content(task.title)
+    expect(page).to have_content(other_task.title)
+    expect(page).to have_content(favorite)
   end
 end
