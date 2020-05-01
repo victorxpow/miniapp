32b2013b4471e9be9af69e374c3047ad3fca4136
João Victor <victorpow@live.com>
2020-05-01 19:59:02 -0300

    list and task style index with vue

---
+++ app/javascript/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="row">
+    <div v-for="list in original_lists" class="col-3">
+      <h6> {{ list.name }}</h6>
+      <hr />
+      <div v-for="(task, index) in list.tasks" class="card card-body">
+        {{ task.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['original_lists'],
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,22 @@ require('jquery')
 //= require jquery_ujs
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+window.store = {}
+
+document.addEventListener("turbolinks:load", function() {
+  var element = document.querySelector("#boards")
+  if (element != undefined) {
+    window.store.lists = JSON.parse(element.dataset.lists)
+
+    const app = new Vue({
+      el: element,
+      data: window.store,
+      template: "<App :original_lists='lists' />",
+      components: { App }
+    })
+  }
+});
